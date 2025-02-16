3062a7656525c6e5ce600195b67533bb81fffe86
Oleh Ohiichuk <chaotichoice@gmail.com>
2025-02-16 11:20:55 +0200

    The module for typing the Project items has been implemented
    - Data Types: This module defines various data types used throughout the project, including type aliases for `ChatID`, `BotIndex`, `UserID`, `LastMessageAuthors`, and `LastMessageTime`.
    
    - BotProtocol: A protocol that outlines the expected behavior of a bot, including methods for starting the bot and sending initial messages. This enhances type checking and ensures that all bot implementations adhere to a consistent interface.
    
    - ChatConfig: A TypedDict that specifies the configuration structure for a chat, including the invite link and prompt.
    
    - BotConfig: A TypedDict that defines the configuration structure for a bot, including phone number, API ID, API hash, and session string.
    
    - Type Aliases: Introduced type aliases for better readability and maintainability, making it easier to understand the types used in the project.

---
+++ core/types.py
@@ -0,0 +1,59 @@
+"""Data types for the project.
+
+This module contains definitions of data types used in the project,
+including protocols for bots and Telegram clients, as well as schemas for bot and chat configurations.
+"""
+
+from typing import TYPE_CHECKING, Protocol, TypeAlias
+from typing_extensions import TypedDict
+
+
+if TYPE_CHECKING:
+    from pyrogram import Client
+
+ChatID: TypeAlias = int
+BotIndex: TypeAlias = int
+UserID: TypeAlias = int
+LastMessageAuthors: TypeAlias = dict[ChatID, set[BotIndex]]
+LastMessageTime: TypeAlias = dict[ChatID, float]
+
+
+class BotProtocol(Protocol):
+    """Protocol for a bot."""
+
+    name: str
+    client: "Client"
+    bot_index: BotIndex
+    flood_wait_until: float
+
+    async def start(self) -> None:
+        """Starts the bot."""
+
+    async def send_initial_message(self, chat_id: int, invite_link: str) -> None:
+        """Sends an initial message to the chat.
+
+        Args:
+            chat_id (int): The ID of the chat where the message will be sent.
+            invite_link (str): The invite link for the chat.
+        """
+
+
+class ChatConfig(TypedDict):
+    """Configuration for a chat."""
+
+    invite_link: str
+    prompt: str
+
+
+class BotConfig(TypedDict):
+    """Configuration for a bot."""
+
+    phone: str
+    api_id: int
+    api_hash: str
+    session: str
+
+
+# Types for configuration
+BotConfigDict = dict[str, str | int]
+ChatConfigDict = dict[str, str]
