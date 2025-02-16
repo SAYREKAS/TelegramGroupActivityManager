6131461923202d0a048d4342dc6a7e3796f80085
Oleh Ohiichuk <chaotichoice@gmail.com>
2025-02-16 16:36:11 +0200

    Refactoring

--- core/project_types.py
+++ core/project_types.py
@@ -5,7 +5,7 @@ including protocols for bots and Telegram clients, as well as schemas
 for bot and chat configurations.
 """
 
-from typing import TYPE_CHECKING, Protocol, TypeAlias
+from typing import TYPE_CHECKING, Protocol, TypeAlias, Literal
 from typing_extensions import TypedDict
 
 
@@ -17,6 +17,51 @@ BotIndex: TypeAlias = int
 UserID: TypeAlias = int
 LastMessageAuthors: TypeAlias = dict[ChatID, set[BotIndex]]
 LastMessageTime: TypeAlias = dict[ChatID, float]
+
+ChatBotModel: TypeAlias = (
+    Literal[
+        "o3-mini",
+        "o3-mini-2025-01-31",
+        "o1",
+        "o1-2024-12-17",
+        "o1-preview",
+        "o1-preview-2024-09-12",
+        "o1-mini",
+        "o1-mini-2024-09-12",
+        "gpt-4o",
+        "gpt-4o-2024-11-20",
+        "gpt-4o-2024-08-06",
+        "gpt-4o-2024-05-13",
+        "gpt-4o-audio-preview",
+        "gpt-4o-audio-preview-2024-10-01",
+        "gpt-4o-audio-preview-2024-12-17",
+        "gpt-4o-mini-audio-preview",
+        "gpt-4o-mini-audio-preview-2024-12-17",
+        "chatgpt-4o-latest",
+        "gpt-4o-mini",
+        "gpt-4o-mini-2024-07-18",
+        "gpt-4-turbo",
+        "gpt-4-turbo-2024-04-09",
+        "gpt-4-0125-preview",
+        "gpt-4-turbo-preview",
+        "gpt-4-1106-preview",
+        "gpt-4-vision-preview",
+        "gpt-4",
+        "gpt-4-0314",
+        "gpt-4-0613",
+        "gpt-4-32k",
+        "gpt-4-32k-0314",
+        "gpt-4-32k-0613",
+        "gpt-3.5-turbo",
+        "gpt-3.5-turbo-16k",
+        "gpt-3.5-turbo-0301",
+        "gpt-3.5-turbo-0613",
+        "gpt-3.5-turbo-1106",
+        "gpt-3.5-turbo-0125",
+        "gpt-3.5-turbo-16k-0613",
+    ]
+    | str
+)
 
 
 class BotProtocol(Protocol):
--- project_config.py
+++ project_config.py
@@ -6,55 +6,14 @@ from typing import Literal
 from pydantic import BaseModel, Field, field_validator, model_validator
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
+from core.project_types import ChatBotModel
+
 
 class ChatBotConfig(BaseModel):
 
     OPEN_API_KEY: str = Field(default="", description="OpenAI API key for accessing LLM")
-    MODEL: (
-        Literal[
-            "o3-mini",
-            "o3-mini-2025-01-31",
-            "o1",
-            "o1-2024-12-17",
-            "o1-preview",
-            "o1-preview-2024-09-12",
-            "o1-mini",
-            "o1-mini-2024-09-12",
-            "gpt-4o",
-            "gpt-4o-2024-11-20",
-            "gpt-4o-2024-08-06",
-            "gpt-4o-2024-05-13",
-            "gpt-4o-audio-preview",
-            "gpt-4o-audio-preview-2024-10-01",
-            "gpt-4o-audio-preview-2024-12-17",
-            "gpt-4o-mini-audio-preview",
-            "gpt-4o-mini-audio-preview-2024-12-17",
-            "chatgpt-4o-latest",
-            "gpt-4o-mini",
-            "gpt-4o-mini-2024-07-18",
-            "gpt-4-turbo",
-            "gpt-4-turbo-2024-04-09",
-            "gpt-4-0125-preview",
-            "gpt-4-turbo-preview",
-            "gpt-4-1106-preview",
-            "gpt-4-vision-preview",
-            "gpt-4",
-            "gpt-4-0314",
-            "gpt-4-0613",
-            "gpt-4-32k",
-            "gpt-4-32k-0314",
-            "gpt-4-32k-0613",
-            "gpt-3.5-turbo",
-            "gpt-3.5-turbo-16k",
-            "gpt-3.5-turbo-0301",
-            "gpt-3.5-turbo-0613",
-            "gpt-3.5-turbo-1106",
-            "gpt-3.5-turbo-0125",
-            "gpt-3.5-turbo-16k-0613",
-        ]
-        | str
-    ) = Field(default="gpt-4o-mini", description="Model for text generation")
-    TEMPERATURE: float = Field(default=0.7, ge=0.0, le=1.0, description="Temperature for generation")
+    MODEL: ChatBotModel = Field(default="gpt-4o-mini", description="Model for text generation")
+    TEMPERATURE: float = Field(ge=0.0, le=1.0, default=0.7, description="Temperature for generation")
     GENERATE_RESPONSE_RULES: str = """
     1. NEVER mention the provided group description
     2. Use the language that is commonly used by the participants in the chat.
@@ -100,7 +59,7 @@ class Settings(BaseSettings):
         env_nested_delimiter="__",
     )
 
-    chat_bot = ChatBotConfig()
+    chat_bot: ChatBotConfig = ChatBotConfig()
 
 
 settings = Settings()
